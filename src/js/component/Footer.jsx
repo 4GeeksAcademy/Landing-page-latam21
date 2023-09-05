7ef0f877f57a91e45e75eb29688a3068f2a907ad
Frossting <109557017+Frossting@users.noreply.github.com>
2023-09-05 16:34:53 +0000

    Finished

---
+++ src/js/component/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <>
+            <div className="container-fluid bg-dark py-4 mt-5">
+                <div className="row">
+                    <div className="col-12">
+                        <h6 className="text-center text-white">Copyright Your Website 2023</h6>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Footer
---
+++ src/js/component/Jumbotron.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Jumbotron() {
+    return (
+        <>
+            <div className="container bg-light py-5">
+                <div className="row">
+                    <div className="col-12">
+                        <h1 className="display-4">A Warm Welcome!</h1>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. A quaerat aspernatur pariatur mollitia dolor. Officia enim dicta harum sint libero! Sunt cumque quae nobis iste. Unde cumque iure fugit earum.</p>
+                        <button className="btn btn-primary">Call to action</button>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Jumbotron
